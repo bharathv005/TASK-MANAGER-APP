fc1454454209677ad00a73454dcd115f30340d30
BHARATH <bharathv0901@gmail.com>
2026-05-25 16:54:01 +0530

    Fix: Update API baseURL to localhost:5000 and add missing frontend dependencies

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-manager-app-j6tn.onrender.com/api",
+  baseURL: "http://localhost:5000/api",
 });
 
 API.interceptors.request.use((req) => {
